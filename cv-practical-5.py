c68ccd55133143a901afa457f00cee448e413ffc
Bjartur Hafþórsson <bjarturhafthors95@gmail.com>
2019-04-03 13:03:48 +0200

    Cleaned up code

--- cv-practical-5.py
+++ cv-practical-5.py
@@ -5,17 +5,8 @@ from keras.models import Sequential
 #from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
 def loadImage(filename):
-    # [1] Get the file category
-
-    # TODO
-    # category = 
-
-    # [2] Load the image in greyscale with opencv.
-
     # Load image with flag "Greyscale"
     image = cv2.imread(filename,0)
-
-    # [3] Find the dimension that is the smallest between the height and the width and assign it to the crop_drim variable.
 
     # Get height and witdh, but skip channels
     height, width = image.shape[:2]
@@ -25,11 +16,7 @@ def loadImage(filename):
       crop_dim = height
     else:
       crop_dim = width
-      
-    #print('h: ' + str(height) + ', w:' + str(width) + ', cd: ' + str(crop_dim))
     
-    # [4] Crop the centre of the image based on the crop_dim dimension for both the height and width.
-
     # Determine centerpoints for width and height
     height_middle = height/2
     width_middle = width/2
@@ -46,16 +33,11 @@ def loadImage(filename):
     # Crop image to lower dimension size from center point
     cropped_image = image[int(height_lower_boundary):int(height_upper_boundary), int(width_lower_boundary):int(width_upper_boundary)]
 
-    # [5] Resize the image to 48 x 48 and divide it with 255.0 to normalise it to floating point format.
-
     # Rezize to 48x48
     resized_image = cv2.resize(cropped_image, (48, 48))
 
-    cv2.imshow('image', resized_image)
-    cv2.waitKey(0)
-
     # Normalize image by dividing with 255 to make all values between 0 and 1
-    #normalized_image = resized_image/255.0
+    normalized_image = resized_image/255.0
     
     #return (normalized_image,label)
 
@@ -79,15 +61,10 @@ def getDatasetLabels(file_list):
 
 ## Part 1: Split filenames into training and test sets
 
-
-
 training_set_files = getDatasetFilenames("data\\image-splits\\train.txt")
 testing_set_files = getDatasetFilenames("data\\image-splits\\test.txt")
 training_set_labels = getDatasetLabels(training_set_files)
 testing_set_labels = getDatasetLabels(testing_set_files)
-
-print(training_set_labels)
-print(testing_set_labels)
 
 ## TODO: Part 2: Load and preprocess images
 
@@ -95,7 +72,7 @@ loadImage("data\\images\\" + training_set_files[0])
 
 ## TODO: Part 3: Construct model
 
-model = Sequential()
+#model = Sequential()
 
 
 ## TODO: Part 4: Train model
